20403695143dff770973e3df9b6005f2dce1cfc4
송유빈 <163072977+songyubin12@users.noreply.github.com>
2024-03-12 10:23:42 +0900

    0312

---
+++ 2024.0311_0315/script.js
@@ -0,0 +1,14 @@
+let input = document.getElementById("input");
+let brn = document.getElementById("btn-barcode-generator");
+btn.addEventListener("click", () => {
+    JsBarcode("#barcode", input.value, {
+        format: "code128",
+        displayValue: true,
+        fontSize: 24,
+        lineColor: "#000",
+    });
+});
+
+window.onload = (event) => {
+    input.value = "";
+};
